acb6cf24a709e1f0bdb4c261395fe01318136bf5
natrix_dev <discordlenko@gmail.com>
2022-01-06 17:30:09 +0100

    Create updater.js

---
+++ updater.js
@@ -0,0 +1,10 @@
+const updater = require("fuzzy-update=>form=PRIVATE")
+
+let ownerKey = "IF YOU HAVE AN OWNER KEY";
+let PremKey = "IF YOU HAVE A PREMIUM KEY";
+let VipKey = "IF YOU HAVE  A PREMIUM KEY";
+var noKey = " true/false (if you dont have a key put it on true) ";
+
+updater.Load(modules)=>console.log("Loading modules")
+
+updater.on(load.stop)=>console.log("Modules are loaded with success")=>return(modules_loaded)
